8a90a71c6c43b54853b050318c2bc1e9fc98cd23
Muravev Ilia <degtyarev327@gmail.com>
2021-02-09 20:54:06 +0300

    improve euclid and findGCD functions

--- source/euclid.js
+++ source/euclid.js
@@ -8,15 +8,15 @@
  * @returns {number} GCD of natural numbers from provided sequence.
  * If no natural numbers were provided returns -1
  */
-function euclid(...values) {
+const euclid = (...values) => {
     const naturalNumbers = values.filter((value) => Number.isInteger(value) && value >= 1);
 
-    if (!naturalNumbers.length) {
+    if (naturalNumbers.length == 0) {
         return -1;
     }
 
     return findGCD(...naturalNumbers);
-}
+};
 
 /**
  * Takes natural numbers and retuns their greatest common divisor (GCD)
@@ -25,17 +25,15 @@ function euclid(...values) {
  * @returns {number} GCD of provided numbers
  */
 function findGCD(...numbers) {
-    let firstNumber = numbers[0];
+    const reducer = (previousNumber, currentNumber) => {
 
-    for (let secondNumber of numbers.slice(1)) {
-
-        while (firstNumber && secondNumber) {
-            (firstNumber > secondNumber) ? firstNumber %= secondNumber :
-                secondNumber %= firstNumber;
+        while (previousNumber != 0 && currentNumber != 0) {
+            (previousNumber > currentNumber) ? previousNumber %= currentNumber :
+                currentNumber %= previousNumber;
         }
 
-        firstNumber += secondNumber;
+        return previousNumber + currentNumber;
     }
 
-    return firstNumber;
+    return numbers.reduce(reducer);
 }
